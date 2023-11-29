6bcbf86f90a07cfe094c8a9448049adbcedad1e3
Narngisa <Thaddark4826@gmail.com>
2023-11-30 00:55:30 +0700

    narngisa

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Calculate-BMI-Python
+# Calculate BMI in Python
 
 The script calculates BMI using the formula: body weight in kilograms divided by the square of height in meters or centimeters divided by 100. It then determines the body mass index (BMI) using the formula Body Weight [kg] / Height [m] squared, and provides information about BMI and associated complications for each value.
--- main.py
+++ main.py
@@ -10,22 +10,32 @@ os.system("cls")
 print("BMI:", bmi)
 
 if bmi < 18.5:
-    print("BMI: Below normal")
-    print("Interpretation: Less than 18.5")
-    print("Risk of complications: Risk of malnutrition")
+    print("""     
+BMI: Below normal
+Interpretation: Less than 18.5
+Risk of complications: Risk of malnutrition
+""")
 elif bmi >= 18.5 and bmi <= 22.9:
-    print("BMI: Normal")
-    print("Interpretation: 18.5-22.9")
-    print("Risk of complications: Lowest risk of complications")
+    print("""
+BMI: Normal
+Interpretation: 18.5-22.9
+Risk of complications: Lowest risk of complications
+""")
 elif bmi >= 23.0 and bmi <= 24.9:
-    print("BMI: Normal")
-    print("Interpretation: 23.0-24.9")
-    print("Risk of complications: Early stage of overweight, starting to have minor complications")
+    print("""
+BMI: Overweight
+Interpretation: 23.0-24.9
+Risk of complications: Early stage of overweight, starting to have minor complications
+""")
 elif bmi >= 25.0 and bmi <= 29.9:
-    print("BMI: Normal")
-    print("Interpretation: 25.0-29.9")
-    print("Risk of complications: Obese, complications in the early stage")
+    print("""
+BMI: Obese
+Interpretation: 25.0-29.9
+Risk of complications: Obese, complications in the early stage
+""")
 elif bmi > 30.0:
-    print("BMI: Below normal")
-    print("Interpretation: More than 30.0")
-    print("Risk of complications: Obese, increased risk of complications due to obesity")
+    print("""
+BMI: Severely obese
+Interpretation: More than 30.0
+Risk of complications: Obese, increased risk of complications due to obesity
+""")
