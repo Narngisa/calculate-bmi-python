255a9ab091f7848f514a483c9ff574fdf1c9d8b9
Narngisa <Thaddark4826@gmail.com>
2023-12-05 15:30:12 +0700

    narngisa

--- main.py
+++ main.py
@@ -1,41 +1,45 @@
 import os
+import json
 
-weight = int(input("Weight: "))
-height = int(input("Height: "))
+# Read JSON file
+file_path = 'data.json'
+with open(file_path, 'r') as file:
+    data = json.load(file)
 
-bmi = weight / (height / 100) ** 2
+below_data = data['below']
+normal_data = data['normal']
+over_data = data['over']
+obese_data = data['obese']
+severely_data = data['severely']
 
-os.system("cls")
+# Input float value for weights and height
+weight = float(input("Weight: "))
+height = float(input("Height: "))
 
-print("BMI:", bmi)
+def calculate(weight, height) -> float:
+    
+    bmi = weight / (height / 100) ** 2
 
-if bmi < 18.5:
-    print("""     
-BMI: Below normal
-Interpretation: Less than 18.5
-Risk of complications: Risk of malnutrition
-""")
-elif bmi >= 18.5 and bmi <= 22.9:
-    print("""
-BMI: Normal
-Interpretation: 18.5-22.9
-Risk of complications: Lowest risk of complications
-""")
-elif bmi >= 23.0 and bmi <= 24.9:
-    print("""
-BMI: Overweight
-Interpretation: 23.0-24.9
-Risk of complications: Early stage of overweight, starting to have minor complications
-""")
-elif bmi >= 25.0 and bmi <= 29.9:
-    print("""
-BMI: Obese
-Interpretation: 25.0-29.9
-Risk of complications: Obese, complications in the early stage
-""")
-elif bmi > 30.0:
-    print("""
-BMI: Severely obese
-Interpretation: More than 30.0
-Risk of complications: Obese, increased risk of complications due to obesity
-""")
+    # Clear Terminal
+    os.system("cls")
+    # Output Title
+    print("BMI: ", bmi)
+
+    if bmi < 18.5:
+        category_data = below_data
+    elif 18.5 <= bmi <= 24.9:
+        category_data = normal_data
+    elif 25.0 <= bmi <= 29.9:
+        category_data = over_data
+    elif 30.0 <= bmi <= 34.9:
+        category_data = obese_data
+    else:
+        category_data = severely_data
+
+    # Output BMI category information
+    print(f"BMI: {category_data['BMI']}")
+    print(f"Interpretation: {category_data['IPT']}")
+    print(f"Risk of complications: {category_data['ROC']}")
+
+# Call the calculate function with the provided weight and height
+calculate(weight, height)
